01bd2fd2e914a153a918626f0a512bcc0a0a8850
uemue <7414847+uemue@users.noreply.github.com>
2022-05-11 23:19:37 +0900

    Change page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Link from 'next/link';
+import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const blogTitle = 'uemue';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{blogTitle}</title>
+      </Head>
+      <header>
+        <p>
+          <Link href='/'>
+            <a>{blogTitle}</a>
+          </Link>
+        </p>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
