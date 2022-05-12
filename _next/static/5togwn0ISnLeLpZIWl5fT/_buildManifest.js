ee4eb1cf9bf46e44f4971811dec95b77a77a4229
uemue <uemue@users.noreply.github.com>
2022-05-12 14:21:41 +0000

    deploy: eb868341c80c772c41538a25efe6ef6eca22f600

---
+++ _next/static/5togwn0ISnLeLpZIWl5fT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0a49f9649b0fabdc.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/articles/[slug]":["static/chunks/pages/articles/[slug]-6cc7431f8f77fad9.js"],sortedPages:["/","/_app","/_error","/articles/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/G_Vb3ReShWwvvhjW7s4Qd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2545f690b1347027.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/articles/[slug]":["static/chunks/pages/articles/[slug]-42492694d8915639.js"],sortedPages:["/","/_app","/_error","/articles/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
